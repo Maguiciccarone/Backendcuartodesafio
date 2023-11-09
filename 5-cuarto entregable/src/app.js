f6383d14162288a9e2fec66800f2647f57bbd693
Magali Ciccarone <magui_lm89@hotmail.com>
2023-11-09 14:49:33 -0300

    cambios del entregable, aun falta

--- 5-cuarto entregable/src/app.js
+++ 5-cuarto entregable/src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import { __dirname } from './utils.js';
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
 
 app.use('/api/products', productRouter);
