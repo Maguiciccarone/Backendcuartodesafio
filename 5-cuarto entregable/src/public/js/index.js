b1b561055f2d39f2585db63c8cd1d435f833d279
Magali Ciccarone <magui_lm89@hotmail.com>
2023-11-10 16:55:26 -0300

    cambios

--- 5-cuarto entregable/src/public/js/index.js
+++ 5-cuarto entregable/src/public/js/index.js
@@ -45,32 +45,3 @@ const SEND = (event) => {
     console.log(newProduct);
 };
 
-// const form = document.getElementById("form");
-// const inputName = document.getElementById("name");
-// const inputPrice = document.getElementById("price");
-// const inputCategory = document.getElementById("category");
-// const products = document.getElementById("products");
-
-// form.onsubmit = (e) => {
-//     e.preventDefault();
-//     const name = inputName.value;
-//     const price = inputPrice.value;
-//     const category = inputCategory.value;
-//     const product = { name, price, category };
-//     socketClient.emit('newProduct', product);
-//     inputName.value = ''
-//     inputPrice.value = ''
-//     inputCategory.value = ''
-// };
-
-// socketClient.on('arrayProducts', (productsArray) => {
-//     let infoProducts = '';
-//     productsArray.forEach(p => {
-//         infoProducts += `${p.name} - $${p.price} - ${p.category} </br>`
-//     })
-//     products.innerHTML = infoProducts
-// })
-
-// socketClient.on('message', (msg) => {
-//     console.log(msg);
-// })
