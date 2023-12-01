c870b946de1b5896d818228ae14bf133e86016c6
Magali Ciccarone <magui_lm89@hotmail.com>
2023-11-30 22:29:51 -0300

    cambios

--- 6- practica integradora/src/daos/mongodb/connection.js
+++ 6- practica integradora/src/daos/mongodb/connection.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-// const MONGO_ATLAS = "mongodb+srv://magaliciccarone:05SoLCqlJ92foihJ@ciccarone.0jpo6y7.mongodb.net/?retryWrites=true&w=majority"
+// const MONGOATLAS = "mongodb+srv://admin:05SoLCqlJ92foihJ@ciccarone.0jpo6y7.mongodb.net/?retryWrites=true&w=majority"
 
-const connectionString = "mongodb://127.0.0.1:27017/coder47345";
+const MONGOCOMPASS = "mongodb://127.0.0.1:27017/ecommerce";
 
 export const initMongoDB = async () => {
     try {
-        await mongoose.connect(connectionString);
+        await mongoose.connect(MONGOCOMPASS);
         console.log('Conectado a la base de datos de MongoDB');
     } catch (error) {
         console.log(`ERROR => ${error}`);
