39c53ddc76840cd6d304ac9fcc6471f83db35f4d
Magali Ciccarone <magui_lm89@hotmail.com>
2023-12-12 22:19:25 -0300

    cambios

--- 7-segunda preentrega/src/app.js
+++ 7-segunda preentrega/src/app.js
@@ -1,17 +1,48 @@
 import './db/connection.js';
 import express from 'express';
 import productRouter from './routes/product.router.js';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import userRouter from './routes/user.router.js';
 import cartRouter from './routes/cart.router.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
 
+const mongoStoreOptions = {
+    store: MongoStore.create({
+        mongoUrl: connectionString,
+        ttl: 120,
+        crypto: {
+            secret: '1234'
+        }
+    }),
+    secret: "1234",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 120000,
+    },
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use(session(mongoStoreOptions));
 
 
 app.use('/products', productRouter);
 app.use('/carts', cartRouter);
+app.use('/users', userRouter);
+app.use('/views', viewsRouter);
 
 app.use(errorHandler);
 
--- 7-segunda preentrega/src/db/connection.js
+++ 7-segunda preentrega/src/db/connection.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
 // const MONGOATLAS = "mongodb+srv://admin:05SoLCqlJ92foihJ@ciccarone.0jpo6y7.mongodb.net/?retryWrites=true&w=majority"
-
-const connectionString = 'mongodb://127.0.0.1:27017/coder47345';
+export const connectionString = "mongodb://127.0.0.1:27017/coder47345";
 
 try {
     await mongoose.connect(connectionString);
-    console.log('Conectado a la base de datos de MongoDB');
+    console.log("Conectado a la base de datos de MongoDB");
 } catch (error) {
     console.log(`ERROR => ${error}`);
 }
